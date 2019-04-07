6e028321de505a4bc8d8dca06e4b59bd01480d58
#Gilla_Dude! <gillarohith1@gmail.com>
2019-04-07 20:12:37 +0530

    _

--- Lab6/server-file.c
+++ Lab6/server-file.c
@@ -8,67 +8,80 @@
 #include<arpa/inet.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+//#include<malloc.h>
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-	int listenfd, connfd;
-	pid_t pid;
-	struct sockaddr_in servaddr;
-	char buff[1024];
-	int counter =0;
-	struct sockaddr_in cliaddr;
-	socklen_t consize = sizeof(servaddr);
-	int fildes;
+	int					listenfd, connfd;
+	struct sockaddr_in	servaddr;
+	char				buff[1024];
+	struct sockaddr_in cliaddr;	
+	socklen_t consize=sizeof(servaddr);	
+	int fildes; 
+
+    pid_t child_pid;
 
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
+	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(6789); /* change this */
+	servaddr.sin_port        = htons(6789);	/* change this */
 
-	char readbuff[1024];
-	int createdfile = creat("newfile", S_IRWXU);
-
-	/*fildes=open(argv[1], O_RDONLY);*/
+        char readbuff[1024];
+        //int createdfile=creat("newfile", S_IRWXU);
+        /*fildes=open(argv[1], O_RDONLY);*/
 
 	int bval;
-
-	bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
+	pid_t cpid;
+	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 	listen(listenfd, 5);
-
-	for (;;)
-	{
+    int client_count=0;
+	char num_str[100];
+	char client_var[1000];
+	for ( ; ; ) 
+    {	
+		printf("\n here waiting for a client\n");
 		connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &consize);
-		if ((pid = fork()) == -1)
-		{
-			close(connfd);
-			continue;
-		}
-		else if(pid > 0){
-			close(connfd);
-			counter++;
-			printf("%d",counter);
-			printf("here2\n");
-			continue;
-		}
-		else if(pid ==0 ){
-			printf("connection received\n");
-			while ((bval = read(connfd, readbuff, 1024)) > 0)
-				write(createdfile, readbuff, bval);
-				close(connfd);
-				printf("%d",counter);
-				// break;			
-		}
-		/*change this */
-		        	
-		
+        client_count=client_count+1;
+        /*change this */
+        printf("connection received\n");
+        int file_no=0;
+		char file_var[100];
+        if ((child_pid=fork())==0)
+        {	
 
-		// close(connfd);
-		// close(listenfd);
-		// close(createdfile);
-		// printf("Done with file receive");
-		// exit(0);
+			file_no=file_no+1;
+            printf ("%s\n","a Child created for dealing with client requests");
+            sprintf(num_str,"%d",client_count);
+			//close(listenfd);
+			sprintf(client_var,"%s","client");
+			strcat(client_var,num_str);
+			strcat(client_var,"_");
+			sprintf(file_var,"%d",file_no);
+			strcat(client_var,file_var);
+			printf("cl_no=%s \n",client_var);
+            int created_new_file=creat(client_var, S_IRWXU);
+            //close listening socket
+            //close (listenfd);
+			//printf("client no:%s",num_str);
+            while((bval=read(connfd, readbuff, 1024))>0) 
+			{
+        	    write(created_new_file, readbuff, bval);
+				//puts(readbuff);
+			}
+
+            printf("Transfer Done\n");
+			close(created_new_file);
+            exit(0);
+        }
+		else
+		{
+			printf("\nwaiting for a client\n");
+			cpid=wait(NULL);
+		}
+		close(connfd);
+
 	}
 }
